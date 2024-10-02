41fcc38278ea31430a69d3f03c7ee8388b417c52
vadde sai kiran <vaddesaikiran8@gmail.com>
2024-10-02 23:08:00 +0530

    in memory auth

---
+++ src/main/java/com/example/CoffeeShop/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.CoffeeShop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	
+	@Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+            http
+              .authorizeHttpRequests(requests -> requests
+                      .requestMatchers("/home", "contact-us").permitAll()
+                      .anyRequest().authenticated())
+              .formLogin(Customizer.withDefaults())
+              .httpBasic(Customizer.withDefaults());
+      return http.build();
+
+  }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("saikiran")
+                .password("{noop}s@123") // {noop} indicates no password encoding
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("kishore")
+                .password("{noop}k@123")
+                .roles("USER")
+                .build();
+
+        UserDetails user3 = User.withUsername("rahul")
+                .password("{noop}r@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+}
